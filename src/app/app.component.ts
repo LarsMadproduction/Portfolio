e055d96b73017f827da66ac28d9273815758c8c7
Lars Schumacher <schumacher90lars@gmail.com>
2024-11-14 02:10:57 +0100

    atf added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
+import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboveTheFoldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
