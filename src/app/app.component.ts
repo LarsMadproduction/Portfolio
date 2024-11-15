39bc017516e0582bcc2bdd287882877f51fd741e
Lars Schumacher <schumacher90lars@gmail.com>
2024-11-15 12:52:43 +0100

    added about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
 import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboveTheFoldComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
