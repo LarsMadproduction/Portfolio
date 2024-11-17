78b124525c491c9751a7c8647c5a6eb0601fcbe8
Lars Schumacher <schumacher90lars@gmail.com>
2024-11-17 09:11:42 +0100

    update contact and added contact form

---
+++ src/app/contact-from/contact-from.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-from',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-from.component.html',
+  styleUrl: './contact-from.component.scss'
+})
+export class ContactFromComponent {
+
+}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFromComponent } from '../contact-from/contact-from.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactFromComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
