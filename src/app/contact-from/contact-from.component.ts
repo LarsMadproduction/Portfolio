ae67dbafc91bafaaf7799aa0b324189ff460037b
Lars Schumacher <schumacher90lars@gmail.com>
2024-11-18 02:40:16 +0100

    added contact form

--- src/app/contact-from/contact-from.component.ts
+++ src/app/contact-from/contact-from.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
 
 @Component({
   selector: 'app-contact-from',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, MatCheckboxModule],
   templateUrl: './contact-from.component.html',
-  styleUrl: './contact-from.component.scss'
+  styleUrl: './contact-from.component.scss',
 })
 export class ContactFromComponent {
 
+  http = inject(HttpClient)
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    checkbox: false
+  };
+
+  checkboxTouched = false;
+  mailTest = true;
+
+  toggleCheckbox() {
+    this.contactData.checkbox = !this.contactData.checkbox;
+    this.checkboxTouched = true;
+  }
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
 }
