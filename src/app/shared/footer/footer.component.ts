0be69a640dd52ba0be6acf9b7c47117f4d47783c
Lars Schumacher <schumacher90lars@gmail.com>
2024-11-19 11:43:22 +0100

    updated portrait and legal notice

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  constructor(private router: Router) {}
 
+  isLegalNoticeRoute = false;
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.isLegalNoticeRoute = this.router.url === '/legal-notice';
+    });
+  }
 }
