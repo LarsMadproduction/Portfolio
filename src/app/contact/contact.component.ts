8aed6ad088c319b54ab197792469aef63882e4ad
Lars Schumacher <schumacher90lars@gmail.com>
2024-11-20 08:43:34 +0100

    contact translation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactFromComponent } from '../contact-from/contact-from.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ContactFromComponent],
+  imports: [ContactFromComponent, TranslateModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
