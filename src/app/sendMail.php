a56da1113c26c3c023f690b4cf18e07ba7e5fc73
Lars Schumacher <schumacher90lars@gmail.com>
2024-12-17 18:06:28 +0100

    changed email

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'schumacher90lars@gmail.com';  
+            $recipient = 'contact@lars-schumacher.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
